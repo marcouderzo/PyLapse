2a6f87f7f6b25981984dadbefbfe48828eca8554
Marco Uderzo <47079771+marcouderzo@users.noreply.github.com>
2022-01-02 20:00:25 +0100

    New code

--- pylapse.py
+++ pylapse.py
@@ -1,24 +1,47 @@
-#Require:
-# https://github.com/loonghao/photoshop-python-api
-# 
+from PIL import ImageGrab
 
-import photoshop.api as ps
-from photoshop import Session
-from pynput.mouse import Listener
-
-ps = Session(action="active_document")
-
-def onClick(x, y, button, pressed):
-        doc = ps.active_document
-        print(doc)
-        options = ps.JPEGSaveOptions(quality=5)
-        jpg = 'C:/Users/marco/Desktop/test.jpg'
-        doc.saveAs(jpg, options, asCopy=True)
-        pass
-
-
-with Listener(on_click=onClick) as listener:
-        listener.join()
+import os, time
+from datetime import datetime
 
 
 
+
+
+def init():
+    mode = input("Please select Run Mode. Type: < capture > to start taking frames, < create > to compile the final timelapse. >> ")
+    retargs=["a", "a", "a", "a"]
+    if mode == "capture":
+        relPath=input("Type Output Folder Name >> ") + '/'
+        cooldownTime=input("Type Time Interval Between Frames (seconds) >> ")
+        retargs[1]= relPath
+        retargs[2]= int(cooldownTime)
+    if mode == "create":
+        fps = input("Type Desired Framerate >> ")  
+        retargs[3]= int(fps)
+
+    retargs[0]= mode
+    return retargs
+
+
+def saveFrame(relPath):
+    im = ImageGrab.grab()
+    dt = datetime.now()
+    fname = "frame_{}.{}.png".format(dt.strftime("%d-%m-%Y %H:%M:%S"), dt.microsecond // 100000)
+    im.save(relPath+fname, 'png')
+
+    
+
+# Main
+
+
+
+args = init()
+
+if args[0] == "capture":
+    print("From Now, Every " + str(args[2]) + " Seconds a Screenshot Will Be Saved.")
+    while True:
+        saveFrame(args[1])
+        time.sleep(args[2])
+
+if args[0] == "create":
+    os.system('ffmpeg -framerate %d -i %s -c:v libx264 -r 20 -pix_fmt yuv420p %s/%d.mp4' % (args[3],"png", "space",int(time.time())))
